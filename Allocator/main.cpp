538b847195f4bcd22559a4e6d4fc69133a8918cb
fengxiaobin <rgfxb@qq.com>
2018-08-02 20:23:55 +0800

    8-2 20.23

--- Allocator/main.cpp
+++ Allocator/main.cpp
@@ -2,6 +2,9 @@
 #include "NN.h"
 #include <iostream>
 #include <vector>
+#include <list>
+#include <algorithm>
+#include <memory>  //auto_ptr
 using namespace std;
 
 int main() {
@@ -23,5 +26,22 @@ int main() {
     ptr=new(ptr) int(10);
     cout<<*(int*)ptr<<endl;
     delete ptr;
+
+    const int array_size=8;
+    int ia[array_size]={0,1,2,3,4,5,6,7};
+    cout<<"+++++++++++++++++"<<endl;
+    list<int> ilist(ia,ia+array_size);                                  //new分配空间
+    cout<<"-----------------"<<endl;
+    list<int>::iterator itr=find(ilist.begin(),ilist.end(),4);
+    if(itr!=ilist.end())
+    {
+        cout<<"find ok"<<endl;
+    }
+    else
+    {
+        cout<<"find error"<<endl;
+    }
+
+    auto_ptr
     return 0;
 }
